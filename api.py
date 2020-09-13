c25e9f015d925a4ea3b9ca9ec9a1e848b3242b7b
Brayden Miller <braydenmiller33@gmail.com>
2020-09-13 03:17:17 -0500

    fixed api issues

--- api.py
+++ api.py
@@ -1,16 +1,9 @@
 import wolframalpha
+import ssl
 
-
-# url = "http://www.wolframalpha.com/queryrecognizer/query.jsp?appid=PY2VE6-7L9AV92EH9&mode=Default&i=Norway"
-# url = "http://www.wolframalpha.com/summaryboxes/v1/query?appid=PY2VE6-7L9AV92EH9&path=countries/e/l5/vw/el"
-# r = requests.get(url).content
-# print(r)
+ssl._create_default_https_context = ssl._create_unverified_context
 client = wolframalpha.Client("PY2VE6-7L9AV92EH9") 
 # Instance of wolf ram alpha  
-
-print("hi")
-# Stores the response from  
-# wolf ram alpha 
 country = input('Country: ') 
 
 res = client.query('What is the population of ' + country + '?')
