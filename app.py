159da65f7dca4ac5d670f31072f092e3e17a7095
Brayden Miller <braydenmiller33@gmail.com>
2020-09-13 03:17:10 -0500

    added wolfram alpha country and states APIs in flask

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import wolframalpha
+import ssl
 
-from flask import Flask, request, render_template
+ssl._create_default_https_context = ssl._create_unverified_context
+
+from flask import Flask, request, render_template, jsonify
 app = Flask(__name__)
 
 client = wolframalpha.Client("PY2VE6-7L9AV92EH9") 
@@ -9,6 +12,44 @@ client = wolframalpha.Client("PY2VE6-7L9AV92EH9")
 def index():
     return render_template('index.html')
 
+@app.route('/stateresponse', methods=['POST', 'GET'])
+def stateresponse():
+    state = request.data.decode()
+    out = ''
+    res = client.query('What is the population of ' + state + '?')
+    answer = next(res.results).text 
+    out += 'Current population: ' + answer + '\n'
+    res = client.query('What is the capital of ' + state + '?')
+    answer = next(res.results).text 
+    out += 'Capital: ' + answer + '\n'
+    res = client.query('What is the state flower of ' + state + '?')
+    answer = next(res.results).text 
+    out += 'State flower: ' + answer + '\n'
+    res = client.query('What are the three largest cities in ' + state + '?')
+    answer = next(res.results).text 
+    out += '\n The three largest cities in this state are:\n' + answer + '\n'
+    return jsonify(out)
+
+@app.route('/countryresponse', methods=['POST', 'GET'])
+def countryresponse():
+    country = request.data.decode()
+    out = ''
+    res = client.query('What is the population of ' + country + '?')
+    answer = next(res.results).text 
+    out += 'Current population: ' + answer + '\n'
+    res = client.query('What is the life expectancy of ' + country + '?')
+    answer = next(res.results).text 
+    out += 'Life expectancy: ' + answer + '\n'
+    res = client.query('What is the capital of ' + country + '?')
+    answer = next(res.results).text 
+    out += 'Capital: ' + answer + '\n'
+    res = client.query('What is the GDP of ' + country + '?')
+    answer = next(res.results).text 
+    out += 'GDP of this country is: ' + answer + '\n'
+    res = client.query('What are the three largest cities in ' + country + '?')
+    answer = next(res.results).text 
+    out += '\n The three largest cities in this country are:\n' + answer + '\n'
+    return jsonify(out)
 
 @app.route('/world_map', methods=['POST', 'GET'])
 def world_map():
