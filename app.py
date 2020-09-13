c6570509486ba83adc0b8e43d8a500677d06449b
Ayush Garg <ayush.garg@duke.edu>
2020-09-12 22:58:43 -0400

    Added menu page

--- app.py
+++ app.py
@@ -7,5 +7,14 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/world_map')
+def world_map():
+    return render_template('world.html')
+
+@app.route('/us_map')
+def us_map():
+    return render_template('us.html')
+
+
 if __name__ == "__main__":
     app.run(debug=True, port=4500)
